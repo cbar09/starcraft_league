30780e8cfa0a7bb210fe4992e58376c9c619d3cb
Carl Barth <carlbarth@imac-27.local>
2012-08-27 10:49:09 -0400

    added static home page view

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,9 @@
 class StaticPagesController < ApplicationController
   def about
   end
+  
+  def home
+    @messages = Message.all
+  end
+  
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,4 @@
 class Message < ActiveRecord::Base
-  attr_accessible :message, :title, :user_id
-  belongs_to :user
+  attr_accessible :message, :title, :user_id, :message_type_id
+  belongs_to :user, :message_types
 end
