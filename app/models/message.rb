62ff7ba462081a70537347b5a2ffd55bf7686299
Carl Barth <carl.barth@gmail.com>
2012-08-27 09:57:17 -0400

    Added Messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,11 @@
 class Message < ActiveRecord::Base
-  attr_accessible :message, :title, :user_id
+  attr_accessible :message, :title, :user_id, :type
   belongs_to :user
+  
+  types = {
+    "warning" => "alert-block",
+    "info" => "alert-info",
+    "success" => "alert-success",
+    "error" => "alert-error"
+  }
 end
