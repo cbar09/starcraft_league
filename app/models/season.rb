e518c7ec66f94630fd464ad91fe51a401c324978
Carl Barth <carl.barth@gmail.com>
2012-09-30 08:41:14 -0400

    Updated Home page with match tags

--- app/models/season.rb
+++ app/models/season.rb
@@ -2,4 +2,9 @@ class Season < ActiveRecord::Base
   attr_accessible :name, :playoff_weeks, :start_date, :weeks
   
   has_many :matches
+  
+  def self.current_week(season_id)
+    day_of_season = (Date.today - Season.find(season_id).start_date).to_i
+    return day_of_season >= 0 ? (day_of_season / 7) + 1 : 0 
+  end
 end
